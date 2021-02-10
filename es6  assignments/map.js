3dd93f244bd0eac4056490fd1d20c28b8e4f0dd8
RASHMII-B <75669118+RASHMII-B@users.noreply.github.com>
2021-02-10 13:57:05 +0530

    Add files via upload

---
+++ es6  assignments/map.js
@@ -0,0 +1,8 @@
+function map(...args) {
+ return args.map((e)=>{
+     return e+1;
+ })
+}
+  
+
+console.log(map(4,9,16,25));
---
+++ es6  assignments/reduce.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+function myFunc(...args) {
+  return args.reduce((total , num)=>{
+return total-num;
+  })
+}
+console.log(myFunc(175, 50, 25));
+
+
+
+
+
+
